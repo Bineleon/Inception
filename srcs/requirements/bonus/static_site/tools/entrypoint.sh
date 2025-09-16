e660f0ba2d0b651607e38fc2276854285b71dd73
neleon <nelbi.leon@gmail.com>
2025-09-16 13:11:53 +0000

    Bonus: ONGOING

--- srcs/requirements/bonus/static_site/tools/entrypoint.sh
+++ srcs/requirements/bonus/static_site/tools/entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+# #!/bin/sh
 
-openssl req -x509 -nodes -newkey rsa:2048 \
-  -keyout /etc/nginx/ssl/game.neleon.42.fr.key \
-  -out /etc/nginx/ssl/game.neleon.42.fr.crt \
-  -subj "/C=FR/ST=Paris/L=Paris/O=42/CN=game.neleon.42.fr" \
-  -days 365
+# openssl req -x509 -nodes -newkey rsa:2048 \
+#   -keyout /etc/nginx/ssl/game.neleon.42.fr.key \
+#   -out /etc/nginx/ssl/game.neleon.42.fr.crt \
+#   -subj "/C=FR/ST=Paris/L=Paris/O=42/CN=game.neleon.42.fr" \
+#   -days 365
 
-exec nginx -g "daemon off;"
+# exec nginx -g "daemon off;"
